b74619551610fd40b9ff63d7de73f9c65641d732
hilden-hw <hildenhw@uw.edu>
2023-05-27 21:42:07 -0700

    sample data table

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,45 @@
-import { Payment, columns } from "./columns"
-import { DataTable } from "./data-table"
+import { Key } from 'react'
+import supabase from '../utils/supabase'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
-async function getData(): Promise<Payment[]> {
+async function getData() {
   // Fetch data from your API here.
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    // ...
-  ]
+  const { data, error } = await supabase
+  .from('PLAYER')
+  .select()
+  if (data != null) {return JSON.parse(JSON.stringify(data))}
 }
 
-export default async function DemoPage() {
-  const data = await getData()
 
-  return (
-    <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={data} />
-    </div>
-  )
+
+export default async function Home() {
+  const results = await getData()
+  return (<Table>
+  <TableCaption>A list of your recent invoices.</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">id</TableHead>
+      <TableHead>name</TableHead>
+      <TableHead className="text-right">is_active</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    {results.map((result: any) => (
+          <TableRow>
+          <TableCell className="font-medium">{result.ID}</TableCell>
+          <TableCell>{result.Name}</TableCell>
+          <TableCell className="text-right">{result.Is_Active}</TableCell>
+        </TableRow>
+    ))}
+  </TableBody>
+</Table>)
+
 }
